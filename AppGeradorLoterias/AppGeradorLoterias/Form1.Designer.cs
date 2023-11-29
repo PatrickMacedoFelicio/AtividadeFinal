9f2e65d2f41e8cf19913b0e9e79b4dd095ee194b
Eloizy Campi <campieloizy80@gmail.com>
2023-11-29 12:41:53 -0300

    buttons

--- AppGeradorLoterias/AppGeradorLoterias/Form1.Designer.cs
+++ AppGeradorLoterias/AppGeradorLoterias/Form1.Designer.cs
@@ -32,13 +32,15 @@
             this.btMega = new System.Windows.Forms.Button();
             this.btLotoFacil = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.btDiaSorte = new System.Windows.Forms.Button();
+            this.btSuperSete = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btQuina
             // 
             this.btQuina.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btQuina.ForeColor = System.Drawing.Color.Blue;
-            this.btQuina.Location = new System.Drawing.Point(79, 220);
+            this.btQuina.Location = new System.Drawing.Point(104, 204);
             this.btQuina.Name = "btQuina";
             this.btQuina.Size = new System.Drawing.Size(179, 42);
             this.btQuina.TabIndex = 29;
@@ -49,7 +51,7 @@
             // 
             this.btMega.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btMega.ForeColor = System.Drawing.Color.Green;
-            this.btMega.Location = new System.Drawing.Point(79, 131);
+            this.btMega.Location = new System.Drawing.Point(104, 112);
             this.btMega.Name = "btMega";
             this.btMega.Size = new System.Drawing.Size(179, 42);
             this.btMega.TabIndex = 28;
@@ -60,7 +62,7 @@
             // 
             this.btLotoFacil.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btLotoFacil.ForeColor = System.Drawing.Color.MediumVioletRed;
-            this.btLotoFacil.Location = new System.Drawing.Point(428, 131);
+            this.btLotoFacil.Location = new System.Drawing.Point(428, 112);
             this.btLotoFacil.Name = "btLotoFacil";
             this.btLotoFacil.Size = new System.Drawing.Size(179, 42);
             this.btLotoFacil.TabIndex = 27;
@@ -72,7 +74,7 @@
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.DarkOrange;
-            this.button1.Location = new System.Drawing.Point(428, 220);
+            this.button1.Location = new System.Drawing.Point(428, 204);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(179, 42);
             this.button1.TabIndex = 30;
@@ -80,11 +82,35 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // btDiaSorte
+            // 
+            this.btDiaSorte.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDiaSorte.ForeColor = System.Drawing.Color.DimGray;
+            this.btDiaSorte.Location = new System.Drawing.Point(104, 297);
+            this.btDiaSorte.Name = "btDiaSorte";
+            this.btDiaSorte.Size = new System.Drawing.Size(179, 42);
+            this.btDiaSorte.TabIndex = 31;
+            this.btDiaSorte.Text = "DIA DE SORTE";
+            this.btDiaSorte.UseVisualStyleBackColor = true;
+            // 
+            // btSuperSete
+            // 
+            this.btSuperSete.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSuperSete.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.btSuperSete.Location = new System.Drawing.Point(428, 297);
+            this.btSuperSete.Name = "btSuperSete";
+            this.btSuperSete.Size = new System.Drawing.Size(179, 42);
+            this.btSuperSete.TabIndex = 32;
+            this.btSuperSete.Text = "SUPER SETE";
+            this.btSuperSete.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(705, 450);
+            this.Controls.Add(this.btSuperSete);
+            this.Controls.Add(this.btDiaSorte);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btQuina);
             this.Controls.Add(this.btMega);
@@ -101,6 +127,8 @@
         private System.Windows.Forms.Button btMega;
         private System.Windows.Forms.Button btLotoFacil;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btDiaSorte;
+        private System.Windows.Forms.Button btSuperSete;
     }
 }
 
