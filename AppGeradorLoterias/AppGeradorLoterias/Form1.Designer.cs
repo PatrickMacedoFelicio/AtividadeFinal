c4c659bff7258cde546406bab9febbad17bc180a
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2023-11-29 00:18:13 -0400

    up

---
+++ AppGeradorLoterias/AppGeradorLoterias/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace AppGeradorLoterias
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btQuina = new System.Windows.Forms.Button();
+            this.btMega = new System.Windows.Forms.Button();
+            this.btLotoFacil = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btQuina
+            // 
+            this.btQuina.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btQuina.ForeColor = System.Drawing.Color.Blue;
+            this.btQuina.Location = new System.Drawing.Point(105, 271);
+            this.btQuina.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btQuina.Name = "btQuina";
+            this.btQuina.Size = new System.Drawing.Size(239, 52);
+            this.btQuina.TabIndex = 29;
+            this.btQuina.Text = "QUINA";
+            this.btQuina.UseVisualStyleBackColor = true;
+            // 
+            // btMega
+            // 
+            this.btMega.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btMega.ForeColor = System.Drawing.Color.Green;
+            this.btMega.Location = new System.Drawing.Point(105, 161);
+            this.btMega.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btMega.Name = "btMega";
+            this.btMega.Size = new System.Drawing.Size(239, 52);
+            this.btMega.TabIndex = 28;
+            this.btMega.Text = "MEGA SENA";
+            this.btMega.UseVisualStyleBackColor = true;
+            // 
+            // btLotoFacil
+            // 
+            this.btLotoFacil.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLotoFacil.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.btLotoFacil.Location = new System.Drawing.Point(571, 161);
+            this.btLotoFacil.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btLotoFacil.Name = "btLotoFacil";
+            this.btLotoFacil.Size = new System.Drawing.Size(239, 52);
+            this.btLotoFacil.TabIndex = 27;
+            this.btLotoFacil.Text = "LOTOFÁCIL";
+            this.btLotoFacil.UseVisualStyleBackColor = true;
+            this.btLotoFacil.Click += new System.EventHandler(this.btLotoFacil_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.button1.Location = new System.Drawing.Point(571, 271);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 52);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "LOTOMANIA";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 554);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btQuina);
+            this.Controls.Add(this.btMega);
+            this.Controls.Add(this.btLotoFacil);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btQuina;
+        private System.Windows.Forms.Button btMega;
+        private System.Windows.Forms.Button btLotoFacil;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AppGeradorLoterias/AppGeradorLoterias/Formularios/FormLotoFacil.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace AppGeradorLoterias.Formularios
+{
+    partial class FormLotoFacil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.lbClass = new System.Windows.Forms.Label();
+            this.lbImpar = new System.Windows.Forms.Label();
+            this.lbPar = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgvNumeros = new System.Windows.Forms.DataGridView();
+            this.btLimpar = new System.Windows.Forms.Button();
+            this.btGerar = new System.Windows.Forms.Button();
+            this.ORD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NÚMERO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNumeros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbClass
+            // 
+            this.lbClass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbClass.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.lbClass.Location = new System.Drawing.Point(109, 318);
+            this.lbClass.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbClass.Name = "lbClass";
+            this.lbClass.Size = new System.Drawing.Size(216, 30);
+            this.lbClass.TabIndex = 38;
+            this.lbClass.Text = "CLASSIFICAÇÃO";
+            // 
+            // lbImpar
+            // 
+            this.lbImpar.AutoSize = true;
+            this.lbImpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbImpar.ForeColor = System.Drawing.Color.Purple;
+            this.lbImpar.Location = new System.Drawing.Point(109, 246);
+            this.lbImpar.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbImpar.Name = "lbImpar";
+            this.lbImpar.Size = new System.Drawing.Size(108, 25);
+            this.lbImpar.TabIndex = 37;
+            this.lbImpar.Text = "ÍMPARES";
+            // 
+            // lbPar
+            // 
+            this.lbPar.AutoSize = true;
+            this.lbPar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPar.ForeColor = System.Drawing.Color.Purple;
+            this.lbPar.Location = new System.Drawing.Point(109, 194);
+            this.lbPar.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbPar.Name = "lbPar";
+            this.lbPar.Size = new System.Drawing.Size(84, 25);
+            this.lbPar.TabIndex = 36;
+            this.lbPar.Text = "PARES";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.label4.Location = new System.Drawing.Point(207, 11);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(175, 31);
+            this.label4.TabIndex = 35;
+            this.label4.Text = "LOTOFÁCIL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(368, 54);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(201, 20);
+            this.label3.TabIndex = 34;
+            this.label3.Text = "NÚMEROS DA SORTE";
+            // 
+            // dgvNumeros
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Plum;
+            this.dgvNumeros.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvNumeros.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvNumeros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNumeros.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ORD,
+            this.NÚMERO});
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvNumeros.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dgvNumeros.Location = new System.Drawing.Point(371, 78);
+            this.dgvNumeros.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvNumeros.Name = "dgvNumeros";
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvNumeros.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.dgvNumeros.RowHeadersWidth = 51;
+            this.dgvNumeros.Size = new System.Drawing.Size(217, 462);
+            this.dgvNumeros.TabIndex = 29;
+            // 
+            // btLimpar
+            // 
+            this.btLimpar.Location = new System.Drawing.Point(35, 112);
+            this.btLimpar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btLimpar.Name = "btLimpar";
+            this.btLimpar.Size = new System.Drawing.Size(267, 28);
+            this.btLimpar.TabIndex = 28;
+            this.btLimpar.Text = "LIMPAR";
+            this.btLimpar.UseVisualStyleBackColor = true;
+            // 
+            // btGerar
+            // 
+            this.btGerar.Location = new System.Drawing.Point(35, 60);
+            this.btGerar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btGerar.Name = "btGerar";
+            this.btGerar.Size = new System.Drawing.Size(267, 28);
+            this.btGerar.TabIndex = 27;
+            this.btGerar.Text = "GERAR NÚMEROS DA SORTE";
+            this.btGerar.UseVisualStyleBackColor = true;
+            // 
+            // ORD
+            // 
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.ORD.DefaultCellStyle = dataGridViewCellStyle3;
+            this.ORD.HeaderText = "ORD";
+            this.ORD.MinimumWidth = 6;
+            this.ORD.Name = "ORD";
+            this.ORD.Visible = false;
+            this.ORD.Width = 125;
+            // 
+            // NÚMERO
+            // 
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.NÚMERO.DefaultCellStyle = dataGridViewCellStyle4;
+            this.NÚMERO.HeaderText = "NÚMERO";
+            this.NÚMERO.MinimumWidth = 6;
+            this.NÚMERO.Name = "NÚMERO";
+            this.NÚMERO.Width = 125;
+            // 
+            // FormLotoFacil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 554);
+            this.Controls.Add(this.lbClass);
+            this.Controls.Add(this.lbImpar);
+            this.Controls.Add(this.lbPar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dgvNumeros);
+            this.Controls.Add(this.btLimpar);
+            this.Controls.Add(this.btGerar);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FormLotoFacil";
+            this.Text = "FormLotoFacil";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNumeros)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbClass;
+        private System.Windows.Forms.Label lbImpar;
+        private System.Windows.Forms.Label lbPar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgvNumeros;
+        private System.Windows.Forms.Button btLimpar;
+        private System.Windows.Forms.Button btGerar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ORD;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NÚMERO;
+    }
+}
